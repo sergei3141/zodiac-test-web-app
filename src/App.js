1dc7f7106290468e597ec4ad8692c9bd6ac9e41b
KulikovNikita97 <sergei3141@mail.ru>
2024-08-23 12:15:49 +0500

    tg bot initialize

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+import {useEffect} from 'react'
+const tg = window.Telegram.WebApp
+
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])  
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div>
+      123
+      <button onClick={onClose}>Close</button>
+    </div>
+  )
+}
+
+export default App;
