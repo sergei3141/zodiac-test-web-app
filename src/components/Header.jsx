4fc1128ab01fa9a8d0dbe61d59d8191193618cd5
KulikovNikita97 <sergei3141@mail.ru>
2024-08-25 00:39:58 +0500

    photo fixed v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const tg = window.Telegram.WebApp;
 
 const Header = ({ userLanguage, changeLanguage }) => {
   const userData = tg?.initDataUnsafe?.user;
+  const userId = userData?.id; 
   const firstName = userData?.first_name || 'Name';
   const [photoUrl, setPhotoUrl] = useState('https://sun1-96.userapi.com/s/v1/ig2/H3p4pdUAc0vEmbkVPrJV9-5q_jx0I8Glhyl7L7shix1JFs_ZtnjAOoDtAkhCmxlFEDQ25MNcju94graeWqlfRttb.jpg?size=50x50&quality=96&crop=8,12,974,974&ava=1');
 
