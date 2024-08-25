4957a87c57b9815f6c524eec9a23dfa859e4dac0
KulikovNikita97 <sergei3141@mail.ru>
2024-08-25 14:18:29 +0500

    isers photo fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,21 @@ const tg = window.Telegram.WebApp;
 
 const Header = ({ userLanguage, changeLanguage }) => {
   const userData = tg?.initDataUnsafe?.user;
-  const userId = userData?.id; 
+  const userId = userData?.id;
   const firstName = userData?.first_name || 'Name';
   const [photoUrl, setPhotoUrl] = useState('https://sun1-96.userapi.com/s/v1/ig2/H3p4pdUAc0vEmbkVPrJV9-5q_jx0I8Glhyl7L7shix1JFs_ZtnjAOoDtAkhCmxlFEDQ25MNcju94graeWqlfRttb.jpg?size=50x50&quality=96&crop=8,12,974,974&ava=1');
 
-  //Получение фото пользователя
   useEffect(() => {
-    const fetchUserProfilePhotos = async () => {
-      try {
-        if (userId) { 
-            console.log(userId)
-          const result = await tg.getUserProfilePhotos(userId); 
-            if (result.photos.length > 0) {
-            // Выбираем фото нужного размера 
-            console.log(result)
-            const photo = result.photos[0][0];
-            const fileId = photo.file_id;
-            
-            // Получаем URL фотографии 
-            const fileUrl = await tg.getFileUrl(fileId);
-            setPhotoUrl(fileUrl);
-            }
-        }
-      } catch (error) {
-        console.error('Ошибка при получении фото профиля:', error);
-      }
-    };
+    // Получаем URL фото из параметров
+    const urlParams = new URLSearchParams(window.location.search);
+    const photoUrlFromBot = urlParams.get('photoUrl');
+    console.log("URL from bot:", photoUrlFromBot) // Проверяем в консоли
 
-    if (userData) {
-      fetchUserProfilePhotos();
+    // Если URL получен, обновляем состояние
+    if (photoUrlFromBot) {
+      setPhotoUrl(photoUrlFromBot);
     }
-}, [userData, userId]); 
+  }, []); // Выполняем useEffect только один раз при монтировании
 
   return (
     <header className="header">
