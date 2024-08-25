ecd846e828c81dd8ce64fe0db12f25128103bef7
KulikovNikita97 <sergei3141@mail.ru>
2024-08-25 14:45:29 +0500

    user id for photo fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,19 @@ const tg = window.Telegram.WebApp;
 
 const Header = ({ userLanguage, changeLanguage }) => {
   const userData = tg?.initDataUnsafe?.user;
-  // const userId = userData?.id;
+  const userId = userData?.id;
   const firstName = userData?.first_name || 'Name';
   const [photoUrl, setPhotoUrl] = useState('https://sun1-96.userapi.com/s/v1/ig2/H3p4pdUAc0vEmbkVPrJV9-5q_jx0I8Glhyl7L7shix1JFs_ZtnjAOoDtAkhCmxlFEDQ25MNcju94graeWqlfRttb.jpg?size=50x50&quality=96&crop=8,12,974,974&ava=1');
 
   useEffect(() => {
-    // Получаем URL фото из параметров
-    const urlParams = new URLSearchParams(window.location.search);
-    const photoUrlFromBot = urlParams.get('photoUrl');
-    console.log("URL from bot:", photoUrlFromBot) // Проверяем в консоли
-
-    // Если URL получен, обновляем состояние
-    if (photoUrlFromBot) {
-      setPhotoUrl(photoUrlFromBot);
+    // ... остальной код ...
+  
+    // Отправляем userId боту
+    if (userId) {
+      tg.sendMessage(userId); 
+      console.log("Отправлен userId:", userId);
     }
-  }, []); // Выполняем useEffect только один раз при монтировании
+  }, []); 
 
   return (
     <header className="header">
